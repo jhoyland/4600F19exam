6238259e406eae819ce5383e02e434f59eca7480
U-PHYSICSR305\Advanced Student <Advanced Student@PhysicsR305.kwantlen.ca>
2019-12-09 09:33:30 -0800

    problem 1 finished

--- prob1/main.c
+++ prob1/main.c
@@ -9,12 +9,41 @@
 	The binary representation should be in groups of 8-bits with a space between. E.g. 01001101 01100101*/
 
 
-void main()
-{
+char main(int argc, char** argv )
+{	
+	printf("program start \n");
+	fflush(stdout);
+	int number; 
+	int i,k,andmask;
+	int n;
 
+	if (argc > 1)
+	{
+		for(n=1;n<argc;n++){
+		sscanf(argv[n],"%d",&number); // if there is a input, read in the first value of the input value
+		printf( "the number of %d binary:",number); 
+		for(i=8;i>=0;i--)
+		{
+        	andmask = 1 << i;
+        	k = number & andmask;
 
+       		k == 0 ? printf("0") : printf("1"); // check the number is overflow to determine print 1 or 0
+   		}
+   		printf("  ");
+   	}
 
+	}
 	
+	else
+	{
+		printf( "does not have a input");
+	}
+
+
+	return 0; 	
 }
 
+   
+
+    
 
