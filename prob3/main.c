8f616509a0aa40a2f749013be7f5439e828df3f9
U-PHYSICSR310\Student <Student@PhysicsR310.kwantlen.ca>
2019-12-09 11:35:00 -0800

    Finished problem

--- prob3/main.c
+++ prob3/main.c
@@ -20,15 +20,70 @@
 
 */
 
-
-
-
-void main()
+int main() // Main Function
 {
 
+    char fname[128];
+    int count=20;
+    float x[count];
+  
+    FILE *input_file, *output_file;
+
+    printf("Enter .dat file name: \n"); //Enter file name
+    gets(fname);
+
+	input_file = fopen(fname,"r"); //Opens/reads input file name
+
+    if (input_file == NULL)     //Prints out error message
+    {
+         printf("\nError! opening file");
+         exit(1);
+    }
+
+    output_file = fopen("output.dat","w"); //Opens output file 
+	
+	if (output_file == NULL)    //Prints out error message
+    {
+         printf("\nError! opening file");
+         exit(1);
+    }
 
 
 
+
+
+	for (int i = 0; i <= 16; ++i)         //range of values between 0 and 16
+	{   
+	    printf("=\n");                    // output bar symbol
+        fscanf(input_file, "%f",&x[i]);   //scans input file
+		fprintf(output_file,"%f\n",x[i]); //prints output to output file
+	}
+
+	
+	for (int i = 17; i < 32; ++i)         //range of values between 17 and 32
+	{   
+		printf("=\n");  
+        fscanf(input_file, "%f",&x[i]);   //scans input file
+		fprintf(output_file,"%f\n",x[i]); //prints output to output file
+	}
+
+
+	for (int i = 33; i < 48; ++i)         //range of values between 33 and 48
+	{   
+		printf("=\n");  
+        fscanf(input_file, "%f",&x[i]);  //scans input file
+		fprintf(output_file,"%f\n",x[i]); //prints output to output file
+	}
+
+
+	
+
+
+	fclose(input_file);    //closes input file
+ 	fclose(output_file);   //closes output file
+	printf("Data output successful!\n");
 }
+
+
 
 
