1a98e704c7ddceb9779ca5c16da152f3316fea46
Ryley Morgan <ryley-morgan@github.com>
2019-12-09 12:34:22 -0800

    Finished Exam

--- prob3/main.c
+++ prob3/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define DATA_FORMAT "%i"
+
 /*
 3) histogram data
 
@@ -21,14 +23,56 @@
 */
 
 
-
-
 void main()
 {
+    int startBin[16], endBin[16], vals[16]={0};
+    
+    // Read in data from file
+    FILE * input_file;
+    input_file = fopen("data.dat","r");
+    int i = 0;
+    int data[5000];
+    while(fscanf(input_file,DATA_FORMAT,&data[i]) != EOF){
+        printf(DATA_FORMAT"\n",data[i]);
+        i++;
+    }
+    fclose(input_file);
 
+    // Create bins
+    for (int j = 0; j < 16; ++j)
+    {
+        startBin[j]=16*j;
+        endBin[j]=16*(j+1)-1;
+    }
 
+    // Sort values based on bins
+    for (int k = 0; k <= i; ++k)
+    {
+        int binFit = (int) data[k]/16;
+        ++vals[binFit];
+    }
 
+    // Save data and print 
+    FILE * output_file;
+    output_file = fopen("output_data.dat","w");
+    for (int l = 0; l < 16; ++l)
+    {
+        int check = fprintf(output_file, "%i\t%i\t%i\n", startBin[l], endBin[l],vals[l]);
+        if (check <= 0)
+        {
+            printf("Error writting data to file: output_data.dat\n");
+            return;
+        }
 
+        // Plot histogram where each "=" represents 10 occurances
+        printf("%i\t-\t%i\t%i\t",startBin[l], endBin[l],vals[l]);
+        for (int m = 0; m < (int)(vals[l]/10); ++m)
+        {
+            printf("=");
+        }
+        printf("\n");
+    }
+    fclose(output_file);
 }
 
 
