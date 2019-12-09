2231b89a49fcb17202e0303af52894bfa77ff208
Alex Chen <weiyu.chen4@email.kpu.ca>
2019-12-09 10:16:10 -0800

    finished problem2

--- prob1/main.c
+++ prob1/main.c
@@ -11,7 +11,52 @@
 
 void main()
 {
+	float inputx;
 
+	printf("Please enter a number in decimal form\n"); //ask user to enter a number
+	fflush(stdout); //print out above printf immediately
+
+	int corr = scanf(" %f",&inputx); //scan the input
+
+	while(corr != 1) //check input correction
+	{
+		if(corr != 1)
+		{
+		printf("Wrong input, try again\n");
+		fflush(stdin);
+		corr = scanf(" %f",&inputx);
+		}
+	}
+	
+	printf("The input was %0.3f\n",inputx);
+
+
+	for(int i=0;i<17;i++)
+	{
+		printf("%x\n",inputx);
+	}
+
+	/*unsigned i; 
+    for (i = 1 << 31; i > 0; i = i / 2)
+    {
+    	if(inputx==1) printf("1");
+    	if(inputx==0) printf("0");
+    }*/
+
+	/*unsigned char inputs = unsigned char inputx;
+    unsigned char byte;
+    int i, j;
+
+    for (i=sizeof(inputx)-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (inputs[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }*/
+
+	
 
 
 	
--- prob2/main.c
+++ prob2/main.c
@@ -11,16 +11,38 @@
 */
 
 
-int main(int argc, char** argc)
+int main(int argc, char** argv)
 {
 	/* Read in coefficients */
-
+	if(argc < 4) //check if 3 coefficient are all entered
+	{
+		
+	}
+	float coe1 = atof(argv[1]); //coefficient for x^2
+	float coe2 = atof(argv[2]); //coefficient for x
+	float coe3 = atof(argv[3]); //coefficient for constant
+	
 
 	/* Find roots */ 
+	float check=pow(coe2,2)-4*coe1*coe3; //check when roots are complex
+	
 
+	float root1 = (-coe2 + sqrt(check))/(2*coe1); //calculate root if they are not complex
+	float root2 = (-coe2 - sqrt(check))/(2*coe1);
 
 
 	/* Print out roots */
+	if(check<0) //complex x+-iy
+	{
+		float firstnum = -coe2/(2*coe1);
+		float secondnum = sqrt(-check)/(2*coe1);
+		printf("Root1 = %f + i%f\nRoot2 = %f - i%f\n",firstnum,secondnum);
+	}
+	if(check>0) //not complex
+	{
+		printf("Root1= %f\nRoot2= %f\n",root1,root2);
+	}
+	
 }
 
 
